5ebb5ad2e106e1f02d569c67db95347f3c22ec97
raranguren <ricaragas@hotmail.com>
2016-10-09 14:20:53 +0200

    4.0
    now redirects to the magnet link that has the most seeders in the search
    results

--- Episodecalendar_Ahoy.user.js
+++ Episodecalendar_Ahoy.user.js
@@ -1,29 +1,27 @@
 // ==UserScript==
-// @name           			Episodecalendar Ahoy
-// @namespace     			n/a
-// @author					Ricardo Aranguren
-// @tester					Ana García
-// @description				Adds links to torrent and subs into episodecalendar.com
+// @name         Episodecalendar Ahoy
+// @namespace    n/a
+// @description	 Adds torrent downloads to episodecalendar.com
 	
-// @version        			3.1
-// @date           			2016/09/05
-// @grant                   none
+// @version      4.0
+// @date         2016/09/10
+// @grant        none
 
-// @include        			http://*episodecalendar.com/calendar*
-// @include        			http://*episodecalendar.com/en/calendar*
-// @include							https://*episodecalendar.com/calendar*
-// @include							https://*episodecalendar.com/en/calendar*
+// @include      http*://episodecalendar.com/*calendar*
+// @include      http*://extratorrent.cc*
 
 // ==/UserScript==
-var proxy = "http://thepiratebay.com.ua/"
-var englishSubs = true // change to get links to english subs instead of Spanish
-var icon = [ "http://i59.tinypic.com/2w3pd38.jpg", "http://www.subtitulos.es/images/icon-translate.png", "http://www.addic7ed.com/images/subtitle.gif"]
-var showName; var showTip; var numbersFound; var season; var episode; var episodeHref; var i;
-var shows = document.getElementsByClassName("show");
-var episodes = document.getElementsByClassName("episode")
-var boxes = document.getElementsByClassName("checkbox-wrapper")
 
-if (boxes.length) for (var j in boxes) if (j>0) { i = j-1;
+searchURL = 'http://extratorrent.cc/search/?search=*'
+magnetIcon = "http://images4et.com/images/magnet.png"
+
+// This part runs in episodecalendar.com
+//////////////////////////////////////////
+
+shows = document.getElementsByClassName("show");
+episodes = document.getElementsByClassName("episode")
+boxes = document.getElementsByClassName("checkbox-wrapper")
+if (boxes.length) for (j in boxes) if (j>0) { i = j-1;
 	//get the show name
 	showName = shows[i].innerHTML;
 	//get the episode ID and convert into format: s00e00
@@ -35,28 +33,31 @@ if (boxes.length) for (var j in boxes) if (j>0) { i = j-1;
     //clean titles
     episodes[i].childNodes[0].innerHTML = ""
 	//add link to torrent search with format: episodename s00e00
-	episodeHref = 'http://extratorrent.cc/search/?search='+ episodeName(showName,season,episode, false)
-	episodes[i].appendChild(imageLink(episodeHref,icon[0]));
-	if (englishSubs) {
-		//add link to addic7ed.com with format: episodename s00e00
-		//episodeHref = 'http://www.addic7ed.com/search.php?search='+ episodeName(showName,season,episode, false);
-		//episodes[i].appendChild(imageLink(episodeHref,icon[2]));
-	} else {
-		//add link to subtitulos.es with format: episodename 0x00
-		episodeHref = 'http://www.subtitulos.es/search.php?cx=partner-pub-9712736367130269%3A9775425900&cof=FORID%3A10&q=';
-		episodeHref += episodeName(showName,season,episode, true)
-		episodes[i].appendChild(imageLink(episodeHref,icon[1]));
-	}
-	//shorten names
-	//showName = showName.replace("The ","'");
-	//if (showName.length > 9) shows[i].innerHTML = showName.substr(0,9)+'...';
-    //align right
-    //episodes[i].style.cssFloat="right"
-    //boxes[j].style.cssFloat="left"
-    //boxes[j].style.marginRight="6px"
-    //boxes[j].style.top="3px"
+	episodeHref = searchURL.replace("*",episodeName(showName,season,episode, false))
+	episodes[i].appendChild(imageLink(episodeHref,magnetIcon));
 }
 
+// This part runs in extratorrent.cc when there is a list of seeders
+////////////////////////////////////////////////////////////////////
+
+seeds = document.getElementsByClassName("sy")
+minSeeds = 1000
+if (seeds.length) for (i in seeds) if (seeds[i].innerHTML > minSeeds) {
+	//for each <td class="sy"> that has a number greater than the previously found
+	minHref = seeds[i].parentNode.getElementsByClassName("tli")[0].getElementsByTagName("a")[0].href
+}
+//redirect if there was a good torrent found
+if (typeof(minHref) == "string") window.location.replace(minHref)
+
+// This part finds magnet links and runs them
+/////////////////////////////////////////////
+
+links = document.getElementsByTagName("a")
+for (i in links) if (links[i].href.substring(0,7) == "magnet:") 
+		window.location.replace(links[i].href)
+
+// functions
+////////////
 
 function episodeName(show, season, episode, x = false) {
 	if (x) {
